d2765a2db83aa98e50b59a33108e7bbda6ce20f5
Igor Shuliakov <igor.shuliakov@nav.no>
2023-03-07 12:33:19 +0100

    respond utils

--- src/main/kotlin/dp/oppdrag/utils/ApiUtils.kt
+++ src/main/kotlin/dp/oppdrag/utils/ApiUtils.kt
@@ -1,6 +1,25 @@
 package dp.oppdrag.utils
 
+import com.papsign.ktor.openapigen.route.response.OpenAPIPipelineResponseContext
+import io.ktor.http.*
+
 
 fun isCurrentlyRunningOnNais(): Boolean {
     return System.getenv("NAIS_APP_NAME") != null
+}
+
+suspend inline fun <reified TResponse : Any> OpenAPIPipelineResponseContext<TResponse>.respondError(message: String) {
+    responder.respond(
+        HttpStatusCode.InternalServerError,
+        message,
+        this.pipeline
+    )
+}
+
+suspend inline fun <reified TResponse : Any> OpenAPIPipelineResponseContext<TResponse>.respondConflict(message: String) {
+    responder.respond(
+        HttpStatusCode.Conflict,
+        message,
+        this.pipeline
+    )
 }
