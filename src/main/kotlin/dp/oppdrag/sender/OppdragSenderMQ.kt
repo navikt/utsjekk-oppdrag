45e455c09348d8eb16823e4c646b96dd8f8ca4af
Igor Shuliakov <igor.shuliakov@nav.no>
2023-03-15 14:31:50 +0100

    Sender bruker createQueueConnection

--- src/main/kotlin/dp/oppdrag/sender/OppdragSenderMQ.kt
+++ src/main/kotlin/dp/oppdrag/sender/OppdragSenderMQ.kt
@@ -1,22 +1,18 @@
 package dp.oppdrag.sender
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
-import com.ibm.mq.constants.CMQC
 import com.ibm.mq.jms.MQQueue
-import com.ibm.msg.client.jms.JmsConstants
-import com.ibm.msg.client.jms.JmsFactoryFactory
-import com.ibm.msg.client.wmq.WMQConstants
 import dp.oppdrag.defaultLogger
+import dp.oppdrag.utils.createQueueConnection
 import no.trygdeetaten.skjema.oppdrag.Oppdrag
 import javax.jms.*
 
 
 class OppdragSenderMQ : OppdragSender {
 
-    private lateinit var producer: MessageProducer
-    private lateinit var connection: Connection
-    private lateinit var session: Session
-    private lateinit var destination: Destination
+    private lateinit var queueConnection: QueueConnection
+    private lateinit var queueSession: QueueSession
+    private lateinit var queueSender: QueueSender
 
     override fun sendOppdrag(oppdrag: Oppdrag): String {
         val xmlMapper = XmlMapper()
@@ -35,63 +31,44 @@ class OppdragSenderMQ : OppdragSender {
         }
 
         try {
-            // Create a connection factory
-            val ff = JmsFactoryFactory.getInstance(WMQConstants.WMQ_PROVIDER)
-            val cf = ff.createConnectionFactory()
-
-            // Set the properties
-            cf.setStringProperty(WMQConstants.WMQ_HOST_NAME, System.getenv("MQ_HOSTNAME"))
-            cf.setIntProperty(WMQConstants.WMQ_PORT, System.getenv("MQ_PORT").toInt())
-            cf.setStringProperty(WMQConstants.WMQ_CHANNEL, System.getenv("MQ_CHANNEL"))
-            cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT)
-            cf.setStringProperty(WMQConstants.WMQ_QUEUE_MANAGER, System.getenv("MQ_QUEUEMANAGER"))
-            cf.setBooleanProperty(WMQConstants.USER_AUTHENTICATION_MQCSP, true)
-            cf.setStringProperty(WMQConstants.USERID, System.getenv("MQ_USER"))
-            cf.setStringProperty(WMQConstants.PASSWORD, System.getenv("MQ_PASSWORD"))
-            cf.setIntProperty(JmsConstants.JMS_IBM_ENCODING, CMQC.MQENC_NATIVE)
-            cf.setIntProperty(JmsConstants.JMS_IBM_CHARACTER_SET, JmsConstants.CCSID_UTF8)
-
             // Create JMS objects
-            connection = cf.createConnection()
-            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
-            destination = session.createQueue(System.getenv("MQ_QUEUE"))
-            producer = session.createProducer(destination)
-
-            // Start the connection
-            connection.start()
+            val queue = MQQueue(System.getenv("MQ_QUEUE"))
+            queueConnection = createQueueConnection()
+            queueSession = queueConnection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE)
+            queueSender = queueSession.createSender(queue)
 
             // Create a message
-            val message = session.createTextMessage(oppdragXml)
+            val message = queueSession.createTextMessage(oppdragXml)
             message.jmsReplyTo = MQQueue(System.getenv("MQ_MOTTAK")) // kvitteringsKø
 
             // Send the message
-            producer.send(message)
+            queueSender.send(message)
 
         } catch (e: Exception) {
             defaultLogger.error { "Klarte ikke sende Oppdrag til OS. Feil: $e" }
             throw e
         } finally {
-            if (::producer.isInitialized) {
+            if (::queueSender.isInitialized) {
                 try {
-                    producer.close()
+                    queueSender.close()
                 } catch (jmsex: JMSException) {
-                    defaultLogger.warn { "Producer could not be closed." }
+                    defaultLogger.warn { "queueSender could not be closed." }
                 }
             }
 
-            if (::session.isInitialized) {
+            if (::queueSession.isInitialized) {
                 try {
-                    session.close()
+                    queueSession.close()
                 } catch (jmsex: JMSException) {
-                    defaultLogger.warn { "Session could not be closed." }
+                    defaultLogger.warn { "queueSession could not be closed." }
                 }
             }
 
-            if (::connection.isInitialized) {
+            if (::queueConnection.isInitialized) {
                 try {
-                    connection.close()
+                    queueConnection.close()
                 } catch (jmsex: JMSException) {
-                    defaultLogger.warn { "Connection could not be closed." }
+                    defaultLogger.warn { "queueConnection could not be closed." }
                 }
             }
         }
