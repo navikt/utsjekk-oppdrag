3457d1b5a1d1da30d43af59c1248bcaffbd0a082
Igor Shuliakov <igor.shuliakov@nav.no>
2023-04-10 10:38:34 +0200

    utbetalingsoppdragExample klassifisering og utbetalesTil

--- src/main/kotlin/dp/oppdrag/api/oppdrag.kt
+++ src/main/kotlin/dp/oppdrag/api/oppdrag.kt
@@ -105,12 +105,12 @@ private val utbetalingsoppdragExample = Utbetalingsoppdrag(
             periodeId = 2L,
             forrigePeriodeId = 1L,
             datoForVedtak = LocalDate.now(),
-            klassifisering = "",
+            klassifisering = "DP",
             vedtakdatoFom = LocalDate.now(),
             vedtakdatoTom = LocalDate.now(),
             sats = BigDecimal.TEN,
             satsType = Utbetalingsperiode.SatsType.DAG,
-            utbetalesTil = "",
+            utbetalesTil = "987654321",
             behandlingId = 3L,
             utbetalingsgrad = 100
         )
