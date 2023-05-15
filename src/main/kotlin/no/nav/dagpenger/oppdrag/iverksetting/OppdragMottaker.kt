bc7e8cd0aa50be5db7ea21b6347706676db4f805
jobi <jorn.ola.birkeland@nav.no>
2023-05-15 10:06:22 +0200

    Fikser feilende channel-config

--- src/main/kotlin/no/nav/dagpenger/oppdrag/iverksetting/OppdragMottaker.kt
+++ src/main/kotlin/no/nav/dagpenger/oppdrag/iverksetting/OppdragMottaker.kt
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Profile("!e2e & !preprod")
+@Profile("!e2e")
 class OppdragMottaker(
     val oppdragLagerRepository: OppdragLagerRepository,
     val env: Environment
