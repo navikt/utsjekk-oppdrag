4cb86c5591b5b8aeb305dd70bcee7a04102f653b
jobi <jorn.ola.birkeland@nav.no>
2023-05-15 09:25:21 +0200

    Skrur på jms-listener igjen

--- src/main/kotlin/no/nav/dagpenger/oppdrag/iverksetting/OppdragSenderMQ.kt
+++ src/main/kotlin/no/nav/dagpenger/oppdrag/iverksetting/OppdragSenderMQ.kt
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service
 import java.lang.UnsupportedOperationException
 
 @Service
-@Profile("!e2e & !preprod")
+@Profile("!e2e")
 class OppdragSenderMQ(
     val jmsTemplateUtgående: JmsTemplate,
     @Value("\${oppdrag.mq.enabled}") val erEnabled: String,
