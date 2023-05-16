7d01fe913b4b9f62fe2aed4c5dbd646dc9cee381
jobi <jorn.ola.birkeland@nav.no>
2023-05-16 10:45:26 +0200

    Legger tilbake metrikker, og fikser en formateringsfeil nais-config

--- src/main/kotlin/no/nav/dagpenger/oppdrag/service/GrensesnittavstemmingService.kt
+++ src/main/kotlin/no/nav/dagpenger/oppdrag/service/GrensesnittavstemmingService.kt
@@ -45,7 +45,7 @@ class GrensesnittavstemmingService(
 
         LOG.info("Fullført grensesnittavstemming for id: ${avstemmingMapper.avstemmingId}")
 
-        //oppdaterMetrikker(fagsystem, meldinger[1].grunnlag)
+        oppdaterMetrikker(fagsystem, meldinger[1].grunnlag)
     }
 
     private fun oppdaterMetrikker(fagsystem: String, grunnlag: Grunnlagsdata) {
@@ -113,5 +113,6 @@ enum class Fagsystem {
     EFOG,
     EFBT,
     EFSP,
-    KS
+    KS,
+    DP
 }
