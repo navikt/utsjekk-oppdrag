e3a0c28c09613e387ee5d99016a6211b3964ddb1
Stephen Ramthun <stephenramthun@gmail.com>
2024-01-10 09:27:14 +0100

    Svar med statuskode og melding ved feil, ikke html

--- dp-oppdrag-simulering/src/main/kotlin/no/nav/dagpenger/simulering/config/ApiExceptionHandler.kt
+++ dp-oppdrag-simulering/src/main/kotlin/no/nav/dagpenger/simulering/config/ApiExceptionHandler.kt
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.server.ResponseStatusException
 
 @ControllerAdvice
 class ApiExceptionHandler {
@@ -16,4 +17,8 @@ class ApiExceptionHandler {
     @ExceptionHandler(SimulerBeregningFeilUnderBehandling::class)
     fun handleFeilUnderBehandling(throwable: SimulerBeregningFeilUnderBehandling): ResponseEntity<String> =
         ResponseEntity.status(500).body(throwable.faultInfo.errorMessage)
+
+    @ExceptionHandler(ResponseStatusException::class)
+    fun handleResponseStatusException(exception: ResponseStatusException): ResponseEntity<String> =
+        ResponseEntity.status(exception.statusCode).body(exception.reason)
 }
