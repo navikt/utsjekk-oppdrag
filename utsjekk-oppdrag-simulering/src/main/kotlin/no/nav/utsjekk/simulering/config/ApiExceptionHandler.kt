e92ac57d00eac0761dd17ec6c326788033388a4f
Martine Enger <marenger@student.matnat.uio.no>
2024-05-16 16:06:10 +0200

    Logg i exception handlers

--- utsjekk-oppdrag-simulering/src/main/kotlin/no/nav/utsjekk/simulering/config/ApiExceptionHandler.kt
+++ utsjekk-oppdrag-simulering/src/main/kotlin/no/nav/utsjekk/simulering/config/ApiExceptionHandler.kt
@@ -1,17 +1,26 @@
 package no.nav.utsjekk.simulering.config
 
 import no.nav.system.os.tjenester.simulerfpservice.simulerfpservicegrensesnitt.SimulerBeregningFeilUnderBehandling
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class TestExceptionHandler {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
+    }
     @ExceptionHandler(SimulerBeregningFeilUnderBehandling::class)
-    fun handleFeilUnderBehandling(throwable: SimulerBeregningFeilUnderBehandling): ResponseEntity<String> =
-        ResponseEntity.status(500).body(throwable.faultInfo.errorMessage)
+    fun handleFeilUnderBehandling(throwable: SimulerBeregningFeilUnderBehandling): ResponseEntity<String> {
+        logger.warn("Feil i respons fra simulering:", throwable)
+        return ResponseEntity.status(500).body(throwable.faultInfo.errorMessage)
+    }
 
     @ExceptionHandler(Throwable::class)
-    fun handleThrowable(throwable: Throwable): ResponseEntity<String> =
-        ResponseEntity.status(500).body(throwable.message)
+    fun handleThrowable(throwable: Throwable): ResponseEntity<String> {
+        logger.warn("Feil i simulering:", throwable)
+        return ResponseEntity.status(500).body(throwable.message)
+    }
 }
